b263505ad40048d433f8598a1b8a45a72a34ef61
Nour Nagm <nournagm97@gmail.com>
2021-03-27 00:50:08 +0200

    update-piecontroller

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -2,15 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers
 {
     public class PieController : Controller
     {
-        public IActionResult Index()
+        private readonly IPieRepository pieRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
         {
-            return View();
+            this.pieRepository = pieRepository;
+            this.categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(pieRepository.AllPies);
         }
     }
 }
