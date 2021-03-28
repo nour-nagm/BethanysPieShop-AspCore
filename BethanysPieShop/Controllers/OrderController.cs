426115ec16949f45aef6168d98f415c784a0cbfb
Nour Nagm <nournagm97@gmail.com>
2021-03-28 17:08:36 +0200

    adding checkout form

---
+++ BethanysPieShop/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly ShoppingCart shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            this.orderRepository = orderRepository;
+            this.shoppingCart = shoppingCart;
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
